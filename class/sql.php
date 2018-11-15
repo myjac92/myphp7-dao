0c862153a042c082c1f934320c4699ba359f0260
josé Antônio <myjac92@gmail.com>
2018-11-14 23:27:54 -0200

    iniciar projeto

---
+++ class/sql.php
@@ -0,0 +1,30 @@
+<?php
+class sql extends PDO{
+  private $conn;
+  public function __construct(){
+    $this->conn =  new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+  }
+  private function setParams($statement,$parameters= array()){
+    foreach ($parameters as $key => $value) {
+      $this->setParam($key,$value);
+      // code...
+    }
+  }
+  private function setParam($statement,$key,$value){
+    $this->bindParam($key,$value);
+  }
+  public function query($rawQuery,$params=array()){
+    $stmt = $this->conn->prepare($rawQuery);
+    $this->setParams($stmt,$params);
+    $stmt ->execute();
+    return $stmt;
+  }
+  public function select($rawQuery,$params=array()):array{
+    $stmt = $this->query($rawQuery);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once("config.php");
+$sql= new sql();
+$usuario = $sql->select("SELECT * FROM tb_usuario");
+echo json_encode($usuario) ;
+
+ ?>
